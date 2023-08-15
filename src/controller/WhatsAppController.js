5bab61600f5d7ef5eba0fe44308e61b3d0f0504c
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-08-15 12:07:11 -0300

    comments of prototypes

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -21,21 +21,25 @@ class WhatsAppController{
 
     elementsPrototype(){
 
+        // aplica um display none sob o elemento
         Element.prototype.hide = function(){
             this.style.display = 'none';
             return this;
         }
     
+        // volta a mostrar o elemento
         Element.prototype.show = function(){
             this.style.display = 'block';
             return this;
         }
 
+        // aplica um display none sob o elemento
         Element.prototype.toggle = function(){
             this.style.display = (this.style.style === 'none') ? 'block' : 'none'
             return this;
         }
 
+        // aplica diversos eventos ao mesmo elemento
         Element.prototype.on = function(events, fn){
             events.split(' ').forEach(event=>{
 
@@ -44,6 +48,11 @@ class WhatsAppController{
             })
         }
 
+        // aplica css em forma de objeto 
+        // ex: elemento.css = {
+        //   widht:50px
+        //   height:50px
+        //              }
         Element.prototype.css = function(styles){
             for ( let name in styles){
                 this.style[name] = styles[name]
@@ -51,21 +60,25 @@ class WhatsAppController{
             }
         }
 
+        //aplica classe
         Element.prototype.addClass = function(name){
             this.classList.add(name)
             return this;
         }
 
+        //remove classe
         Element.prototype.removeClass = function(name){
             this.classList.remove(name)
             return this;
         }
 
+        //se não tiver a classe, aplica a mesma
         Element.prototype.toggleClass = function(name){
             this.classList.toggle(name)
             return this;
         }
 
+        //pergunta se possui a classe
         Element.prototype.hasClass = function(name){
            return this.classList.contains(name)
         }
