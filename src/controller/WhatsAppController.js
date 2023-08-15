52027cd08180aed5530e99304d63c55b90105197
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-08-15 11:59:51 -0300

    defining prototypes

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -2,6 +2,7 @@ class WhatsAppController{
 
     constructor(){
 
+        this.elementsPrototype();
         this.loadElements(); // método que transformar id do html em camelCase (propriedades de um objeto)
 
     }
@@ -15,6 +16,59 @@ class WhatsAppController{
             this.el[Format.getCamelcase(element.id)] = element; //chamando util Format (app.el)
 
         })
+
+    }
+
+    elementsPrototype(){
+
+        Element.prototype.hide = function(){
+            this.style.display = 'none';
+            return this;
+        }
+    
+        Element.prototype.show = function(){
+            this.style.display = 'block';
+            return this;
+        }
+
+        Element.prototype.toggle = function(){
+            this.style.display = (this.style.style === 'none') ? 'block' : 'none'
+            return this;
+        }
+
+        Element.prototype.on = function(events, fn){
+            events.split(' ').forEach(event=>{
+
+                this.addEventListener(event, fn)
+                return this;
+            })
+        }
+
+        Element.prototype.css = function(styles){
+            for ( let name in styles){
+                this.style[name] = styles[name]
+                return this;
+            }
+        }
+
+        Element.prototype.addClass = function(name){
+            this.classList.add(name)
+            return this;
+        }
+
+        Element.prototype.removeClass = function(name){
+            this.classList.remove(name)
+            return this;
+        }
+
+        Element.prototype.toggleClass = function(name){
+            this.classList.toggle(name)
+            return this;
+        }
+
+        Element.prototype.hasClass = function(name){
+           return this.classList.contains(name)
+        }
 
     }
 
