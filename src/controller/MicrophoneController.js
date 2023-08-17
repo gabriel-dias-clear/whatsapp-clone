0d9ac9f6aa34982d801c543afb35701e9cca30b5
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-08-17 08:40:16 -0300

    microphone controller (capturing microphone audio)

---
+++ src/controller/MicrophoneController.js
@@ -0,0 +1,31 @@
+export class MicrophoneController {
+
+    constructor(){
+
+        navigator.mediaDevices.getUserMedia({
+            audio:true
+        }).then(stream => {
+
+            this._stream = stream;
+
+            let audio = new Audio();
+
+            audio.srcObject = stream;
+
+            audio.play()
+
+        }).catch(err=>{
+            console.log(err)
+        })
+
+    }
+
+    stop(){
+        
+        this._stream.getTracks().forEach(track=>{
+            track.stop();
+        })
+
+    }
+
+}
