5331444b3164a20baa728605e26684ad1bc0450b
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-08-18 11:26:29 -0300

    contact in view renderization

--- models/Model.js
+++ models/Model.js
@@ -8,7 +8,7 @@ export class Model extends ClassEvent{
     }
 
     fromJSON(json){
-        this._data = Object.assign(this._data,json);
+        this._data = Object.assign(this._data, json);
         this.trigger('datachange', this.toJSON())
     }
 
